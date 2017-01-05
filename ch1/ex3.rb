a7131d25d6bafaa17fb9bfbeaf2201ac5727cb93
ppiron <ppiron@gmail.com>
2017-01-05 21:02:52 +0000

    Exercises for chapters 1 and 2

---
+++ ch1/ex3.rb
@@ -0,0 +1,5 @@
+movies = { :m1 => 2000, :m2 => 2001, :m3 => 2002, :m4 => 2003 }
+puts movies[:m1]
+puts movies[:m2]
+puts movies[:m3]
+puts movies[:m4]
---
+++ ch2/name.rb
@@ -0,0 +1,7 @@
+puts "Type in your name"
+name = gets.chomp
+puts "Type in your surname"
+surname = gets.chomp
+puts "Hi " + name + ", how are you?"
+puts name + " " + surname
+10.times { puts name }
