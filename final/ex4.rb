44e205c11e734f0ef5c944bb0e9f854fb8ee6775
ppiron <ppiron@gmail.com>
2017-01-13 21:45:47 +0000

    Final exercises, part1

---
+++ final/ex4.rb
@@ -0,0 +1,5 @@
+ary = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].push(11).insert(0, 0)
+p ary.delete(11)
+ary.push(3)
+p ary
+p ary.uniq!
---
+++ final/ex8.rb
@@ -0,0 +1,4 @@
+hsh = { :milan => "Milan", :inter => "Milan", :juventus => "Turin" }
+hsh1 = { milan: "Milan", inter: "Milan", juventus: "Turin" }
+p hsh
+p hsh1
